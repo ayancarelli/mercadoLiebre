59933491183d721a9d46bf59ed5abdb7c304899c
ayancarelli <alan_yancarelli@hotmail.com>
2022-09-26 15:54:50 -0300

    clase 5 lista

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ app.get ('/', (req,res) => {
     res.sendFile (path.resolve(__dirname,'./views/index.html'))
 });
 
+app.get ('/registro', (req,res) => {
+    res.sendFile (path.resolve(__dirname,'./views/registro.html'))
+});
+
+app.get ('/login', (req,res) => {
+    res.sendFile (path.resolve(__dirname,'./views/login.html'))
+});
+
 app.use (express.static(path.resolve(__dirname,'./public')));
 
 app.listen(3002,() => {
